f77db0431458b4f5f2bda30626459c2968ba330b
Noel <terrorer9999@gmail.com>
2021-12-27 03:34:31 +0800

    docs(app/models/course, chapter): add note

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -3,6 +3,8 @@ class Chapter < ApplicationRecord
   has_many :lessons, -> { order(:order, :id) }, dependent: :destroy, inverse_of: :chapter
 
   validates :title, presence: true
+
+  # we may move this to some kind of form object in the future in order to stay the basic but valid validations in this model, otherwise it might cause some cost when maniplating this object
   validates_associated :lessons
 
   accepts_nested_attributes_for :lessons, allow_destroy: true, reject_if: :all_blank
--- app/models/course.rb
+++ app/models/course.rb
@@ -4,6 +4,8 @@ class Course < ApplicationRecord
 
   validates :title, :mentor_title, presence: true
   validates :title, uniqueness: true
+
+  # we may move this to some kind of form object in the future in order to stay the basic but valid validations in this model, otherwise it might cause some cost when maniplating this object
   validates_associated :chapters
 
   accepts_nested_attributes_for :chapters, allow_destroy: true, reject_if: :all_blank
